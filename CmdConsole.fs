0518cbd2d9bde065b8d38e86be017ce64ac7dc7c
mochida.kohei <mochida.kohei@htb-energy.com>
2022-11-27 23:35:30 +0900

    mainly simplify
    thanks @haxe
    
    see : https://gist.github.com/sayurin/1c1b5e6f3daf1006414a3344156bfcf4

--- CmdConsole.fs
+++ CmdConsole.fs
@@ -1,76 +1,27 @@
-namespace CmdConsole
+namespace Sample
 
-module ColorScheme =
+module DllImported =
 
   open System
   open System.Runtime.InteropServices
 
-  module private Imported =
+  [<Literal>]
+  let STD_OUTPUT_HANDLE = -11
 
-    [<StructLayoutAttribute(LayoutKind.Sequential,Pack=2)>]
-    [<Struct>]
-    type COORD =
-      val mutable x : int16
-      val mutable y : int16
+  [<Literal>]
+  let TMPF_TRUETYPE = 4
 
-    [<StructLayoutAttribute(LayoutKind.Sequential,Pack=2)>]
-    [<Struct>]
-    type SMALL_RECT =
-      val mutable Left   : int16
-      val mutable Top    : int16
-      val mutable Right  : int16
-      val mutable Bottom : int16
+  [<Literal>]
+  let LF_FACESIZE = 32
 
-    type COLORREF = uint
-
-    [<StructLayoutAttribute(LayoutKind.Sequential)>]
-    [<Struct>]
-    type CONSOLE_SCREEN_BUFFER_INFOEX =
-      val mutable cbSize               : int
-      val mutable dwSize               : COORD
-      val mutable dwCursorPosition     : COORD
-      val mutable wAttributes          : int16
-      val mutable srWindow             : SMALL_RECT
-      val mutable dwMaximumWindowSize  : COORD
-      val mutable wPopupAttributes     : int16
-      val mutable bFullscreenSupported : bool
-      val mutable black                : COLORREF
-      val mutable darkBlue             : COLORREF
-      val mutable darkGreen            : COLORREF
-      val mutable darkCyan             : COLORREF
-      val mutable darkRed              : COLORREF
-      val mutable darkMagenta          : COLORREF
-      val mutable darkYellow           : COLORREF
-      val mutable gray                 : COLORREF
-      val mutable darkGray             : COLORREF
-      val mutable blue                 : COLORREF
-      val mutable green                : COLORREF
-      val mutable cyan                 : COLORREF
-      val mutable red                  : COLORREF
-      val mutable magenta              : COLORREF
-      val mutable yellow               : COLORREF
-      val mutable white                : COLORREF
-
-    let STD_OUTPUT_HANDLE = -11
-
-    [<DllImportAttribute("kernel32.dll",SetLastError=true)>]
-    extern IntPtr GetStdHandle(int nStdHndle)
-
-    [<DllImportAttribute("kernel32.dll",SetLastError=true)>]
-    extern IntPtr GetConsoleScreenBufferInfoEx(IntPtr hConsoleOutput, CONSOLE_SCREEN_BUFFER_INFOEX& ConsoleScreenBufferInfo)
-
-    [<DllImportAttribute("kernel32.dll",SetLastError=true)>]
-    extern IntPtr SetConsoleScreenBufferInfoEx(IntPtr hConsoleOutput, CONSOLE_SCREEN_BUFFER_INFOEX& ConsoleScreenBufferInfo)
-
-
-  open Imported
-
+  [<Struct; StructLayout(LayoutKind.Sequential, Size = 4)>]
   type RGB = {
-    Red   : int
-    Green : int
-    Blue  : int
+    Red   : byte
+    Green : byte
+    Blue  : byte
   }
 
+  [<Struct; StructLayout(LayoutKind.Sequential)>]
   type ColorScheme = {
     black       : RGB
     darkBlue    : RGB
@@ -90,42 +41,121 @@ module ColorScheme =
     white       : RGB
   }
 
-  let ToRGB (colorRef:uint) =
-    (
-         colorRef         &&& (uint 0xFF) |> int
-      , (colorRef >>>  8) &&& (uint 0xFF) |> int
-      , (colorRef >>> 16) &&& (uint 0xFF) |> int
-    )
+  [<Struct; StructLayout(LayoutKind.Sequential)>]
+  type COORD = { X : int16; Y : int16 }
 
-  let ToCOLORREF red green blue =
-    let r =  uint red
-    let g = (uint green) <<< 8
-    let b = (uint blue)  <<< 16
-    r + g + b
+  [<Struct; StructLayoutAttribute(LayoutKind.Sequential)>]
+  type SMALL_RECT = { Left : int16; Top : int16; Right : int16; Bottom : int16 }
 
-  let updateColorPalette (colorScheme:ColorScheme) =
+  [<Struct; StructLayoutAttribute(LayoutKind.Sequential)>]
+  type CONSOLE_SCREEN_BUFFER_INFOEX =
+    val mutable cbSize               : int
+    val mutable dwSize               : COORD
+    val mutable dwCursorPosition     : COORD
+    val mutable wAttributes          : int16
+    val mutable srWindow             : SMALL_RECT
+    val mutable dwMaximumWindowSize  : COORD
+    val mutable wPopupAttributes     : int16
+    val mutable bFullscreenSupported : bool
+    val mutable colorScheme          : ColorScheme
 
+  [<Struct>]
+  [<StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)>]
+  type CONSOLE_FONT_INFO_EX =
+      val mutable cbSize: uint32
+      val mutable nFont: uint32
+      val mutable dwFontSize: COORD
+      val mutable FontFamily: int
+      val mutable FontWeight: int
+      [<MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)>]
+      val mutable FaceName: string
+
+  [<DllImportAttribute("kernel32.dll",SetLastError=true)>]
+  extern IntPtr GetStdHandle(int nStdHndle)
+
+  [<DllImportAttribute("kernel32.dll",SetLastError=true)>]
+  extern IntPtr GetConsoleScreenBufferInfoEx(IntPtr hConsoleOutput, CONSOLE_SCREEN_BUFFER_INFOEX& ConsoleScreenBufferInfo)
+
+  [<DllImportAttribute("kernel32.dll",SetLastError=true)>]
+  extern IntPtr SetConsoleScreenBufferInfoEx(IntPtr hConsoleOutput, CONSOLE_SCREEN_BUFFER_INFOEX& ConsoleScreenBufferInfo)
+
+  [<DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)>]
+  extern bool GetCurrentConsoleFontEx(IntPtr consoleOutput, bool maximumWindow, CONSOLE_FONT_INFO_EX lpConsoleCurrentFontEx)
+
+  [<DllImport("kernel32.dll", SetLastError = true)>]
+  extern bool SetCurrentConsoleFontEx(IntPtr consoleOutput, bool maximumWindow, CONSOLE_FONT_INFO_EX consoleCurrentFontEx)
+
+
+module CmdConsole =
+
+  open System
+  open System.Runtime.InteropServices
+  open DllImported
+
+  type FontParams = {
+    Name : string
+    Size : int16
+  }
+
+  type ColorParams = {
+    backGroundColor : int
+    foreGroundColor : int
+  }
+
+  let updateColorScheme (colorScheme:ColorScheme) =
     let mutable screenBuffer = new CONSOLE_SCREEN_BUFFER_INFOEX(
         cbSize = Marshal.SizeOf(typeof<CONSOLE_SCREEN_BUFFER_INFOEX>)
     )
-
     GetConsoleScreenBufferInfoEx(GetStdHandle(STD_OUTPUT_HANDLE), &screenBuffer ) |> ignore
-
-    screenBuffer.black       <- (ToCOLORREF colorScheme.black.Red        colorScheme.black.Green       colorScheme.black.Blue)
-    screenBuffer.darkBlue    <- (ToCOLORREF colorScheme.darkBlue.Red     colorScheme.darkBlue.Green    colorScheme.darkBlue.Blue)
-    screenBuffer.darkGreen   <- (ToCOLORREF colorScheme.darkGreen.Red    colorScheme.darkGreen.Green   colorScheme.darkGreen.Blue)
-    screenBuffer.darkCyan    <- (ToCOLORREF colorScheme.darkCyan.Red     colorScheme.darkCyan.Green    colorScheme.darkCyan.Blue)
-    screenBuffer.darkRed     <- (ToCOLORREF colorScheme.darkRed.Red      colorScheme.darkRed.Green     colorScheme.darkRed.Blue)
-    screenBuffer.darkMagenta <- (ToCOLORREF colorScheme.darkMagenta.Red  colorScheme.darkMagenta.Green colorScheme.darkMagenta.Blue)
-    screenBuffer.darkYellow  <- (ToCOLORREF colorScheme.darkYellow.Red   colorScheme.darkYellow.Green  colorScheme.darkYellow.Blue)
-    screenBuffer.gray        <- (ToCOLORREF colorScheme.gray.Red         colorScheme.gray.Green        colorScheme.gray.Blue)
-    screenBuffer.darkGray    <- (ToCOLORREF colorScheme.darkGray.Red     colorScheme.darkGray.Green    colorScheme.darkGray.Blue)
-    screenBuffer.blue        <- (ToCOLORREF colorScheme.blue.Red         colorScheme.blue.Green        colorScheme.blue.Blue)
-    screenBuffer.green       <- (ToCOLORREF colorScheme.green.Red        colorScheme.green.Green       colorScheme.green.Blue)
-    screenBuffer.cyan        <- (ToCOLORREF colorScheme.cyan.Red         colorScheme.cyan.Green        colorScheme.cyan.Blue)
-    screenBuffer.red         <- (ToCOLORREF colorScheme.red.Red          colorScheme.red.Green         colorScheme.red.Blue)
-    screenBuffer.magenta     <- (ToCOLORREF colorScheme.magenta.Red      colorScheme.magenta.Green     colorScheme.magenta.Blue)
-    screenBuffer.yellow      <- (ToCOLORREF colorScheme.yellow.Red       colorScheme.yellow.Green      colorScheme.yellow.Blue)
-    screenBuffer.white       <- (ToCOLORREF colorScheme.white.Red        colorScheme.white.Green       colorScheme.white.Blue)
-
+    screenBuffer.colorScheme <- colorScheme
     SetConsoleScreenBufferInfoEx(GetStdHandle(STD_OUTPUT_HANDLE), &screenBuffer ) |> ignore
+
+  let updateFont (fontParams:FontParams) =
+    let hnd = GetStdHandle(STD_OUTPUT_HANDLE)
+    let mutable newFontInfo = CONSOLE_FONT_INFO_EX()
+    newFontInfo.cbSize <- uint32 (Marshal.SizeOf(newFontInfo))
+    newFontInfo.FontFamily <- TMPF_TRUETYPE
+    newFontInfo.FaceName <- fontParams.Name
+    newFontInfo.dwFontSize <- {X = int16 1 ; Y = fontParams.Size}
+    SetCurrentConsoleFontEx(hnd, false, newFontInfo) |> ignore
+
+  let consoleEncodingUTF8 (flg:bool) =
+    if flg then
+      Console.InputEncoding   <- Text.UTF8Encoding.UTF8
+      Console.OutputEncoding  <- Text.UTF8Encoding.UTF8
+
+  let consoleColor (colorParams:ColorParams) =
+    match colorParams.backGroundColor with
+    | 0  -> Console.BackgroundColor <- ConsoleColor.Black
+    | 1  -> Console.BackgroundColor <- ConsoleColor.DarkBlue
+    | 2  -> Console.BackgroundColor <- ConsoleColor.DarkGreen
+    | 3  -> Console.BackgroundColor <- ConsoleColor.DarkCyan
+    | 4  -> Console.BackgroundColor <- ConsoleColor.DarkRed
+    | 5  -> Console.BackgroundColor <- ConsoleColor.DarkMagenta
+    | 6  -> Console.BackgroundColor <- ConsoleColor.DarkYellow
+    | 7  -> Console.BackgroundColor <- ConsoleColor.Gray
+    | 8  -> Console.BackgroundColor <- ConsoleColor.DarkGray
+    | 9  -> Console.BackgroundColor <- ConsoleColor.Blue
+    | 10 -> Console.BackgroundColor <- ConsoleColor.Green
+    | 11 -> Console.BackgroundColor <- ConsoleColor.Cyan
+    | 12 -> Console.BackgroundColor <- ConsoleColor.Red
+    | 13 -> Console.BackgroundColor <- ConsoleColor.Magenta
+    | 14 -> Console.BackgroundColor <- ConsoleColor.Yellow
+    | _  -> Console.BackgroundColor <- ConsoleColor.White
+    match colorParams.foreGroundColor with
+    | 0  -> Console.ForegroundColor <- ConsoleColor.Black
+    | 1  -> Console.ForegroundColor <- ConsoleColor.DarkBlue
+    | 2  -> Console.ForegroundColor <- ConsoleColor.DarkGreen
+    | 3  -> Console.ForegroundColor <- ConsoleColor.DarkCyan
+    | 4  -> Console.ForegroundColor <- ConsoleColor.DarkRed
+    | 5  -> Console.ForegroundColor <- ConsoleColor.DarkMagenta
+    | 6  -> Console.ForegroundColor <- ConsoleColor.DarkYellow
+    | 7  -> Console.ForegroundColor <- ConsoleColor.Gray
+    | 8  -> Console.ForegroundColor <- ConsoleColor.DarkGray
+    | 9  -> Console.ForegroundColor <- ConsoleColor.Blue
+    | 10 -> Console.ForegroundColor <- ConsoleColor.Green
+    | 11 -> Console.ForegroundColor <- ConsoleColor.Cyan
+    | 12 -> Console.ForegroundColor <- ConsoleColor.Red
+    | 13 -> Console.ForegroundColor <- ConsoleColor.Magenta
+    | 14 -> Console.ForegroundColor <- ConsoleColor.Yellow
+    | _  -> Console.ForegroundColor <- ConsoleColor.White
